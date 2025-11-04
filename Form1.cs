0419c234e879b71e89f459cc8defbe8f0656a8a5
milos pusic <milos.pusic@prvabeogim.edu.rs>
2025-11-04 15:50:08 +0100

    V 0.2

--- Form1.cs
+++ Form1.cs
@@ -8,24 +8,42 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
+using System.Reflection.Emit;
 
 namespace ednevnik410b
 {
     public partial class Form1 : Form
     {
+        int br_reda;
+        DataTable tabela;
         public Form1()
         {
             InitializeComponent();
         }
 
+        public void popuni_txt()
+        {
+            textBox1.Text = tabela.Rows[br_reda][0].ToString();
+            textBox2.Text = tabela.Rows[br_reda][1].ToString();
+            textBox3.Text = tabela.Rows[br_reda][2].ToString();
+            textBox4.Text = tabela.Rows[br_reda][3].ToString();
+            textBox5.Text = tabela.Rows[br_reda][4].ToString();
+            textBox6.Text = tabela.Rows[br_reda][5].ToString();
+        }
         private void Form1_Load(object sender, EventArgs e)
         {
             SqlConnection veza = new SqlConnection("Data Source=DESKTOP-6LPEK0P\\SQLEXPRESS;Initial catalog=dnevnik410b;Integrated security=true");
             SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM osoba", veza);
-            DataTable tabela = new DataTable();
+            tabela = new DataTable();
             da.Fill(tabela);
-            textBox1.Text = tabela.Rows[0][0].ToString();
-            textBox2.Text = tabela.Rows[0][1].ToString();
+            br_reda = 0;
+            popuni_txt();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            br_reda++;
+            popuni_txt();
         }
     }
 }
