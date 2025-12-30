9724168934bf758e9c35afabbb28f42d58ec25f9
milos pusic <milos.pusic@prvabeogim.edu.rs>
2025-12-30 15:57:05 +0100

    sifarnik 1

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace ednevnik410b
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Upisnica());
+            Application.Run(new sifarnik());
 
         }
     }
---
+++ sifarnik.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ednevnik410b
+{
+    public partial class sifarnik : Form
+    {
+        SqlDataAdapter Adapter;
+        DataTable podaci;
+        public sifarnik()
+        {
+            InitializeComponent();
+        }
+
+        private void sifarnik_Load(object sender, EventArgs e)
+        {
+            Adapter = new SqlDataAdapter("SELECT * FROM predmet", konekcija.povezi());
+            podaci = new DataTable();
+            Adapter.Fill(podaci);
+            dataGridView1.DataSource = podaci;
+        }
+    }
+}
